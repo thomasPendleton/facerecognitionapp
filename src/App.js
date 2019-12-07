814f38984b2f8d17dbb1dadcf3a190820370db53
thomasPendleton <57029458+thomasPendleton@users.noreply.github.com>
2019-12-06 23:27:49 -0800

    second update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import Navigation from './components/Navigation/navigation.js';
-import Logo from './components/Logo/Logo.js'
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js'
+import Logo from './components/Logo/Logo.js';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
+import Rank from './components/Rank/Rank.js';
+import Particles from 'react-particles-js';
 import './App.css';
+
+const particlesOptions = {
+  'particles': {
+    'number': {
+      'value': 100,
+      'density': {
+        'enable': true,
+        'value_area': 500
+      }
+    }
+  },
+//   "interactivity": {
+//     "events": {
+//         "onhover": {
+//             "enable": true,
+//             "mode": "repulse"
+//         }
+//     }
+// }
+}
+
 
 function App() {
   return (
     <div className="App">
+      <Particles className= 'particles'
+                params={particlesOptions} />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/*
       
