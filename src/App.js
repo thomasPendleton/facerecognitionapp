17827ff0158d4a459df228b2c2f8581a7b067ca8
thomasPendleton <57029458+thomasPendleton@users.noreply.github.com>
2019-12-06 22:37:20 -0800

    smart brain project start

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navigation from './components/Navigation/navigation.js';
+import Logo from './components/Logo/Logo.js'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Logo />
+      <ImageLinkForm />
+      {/*
+      
+      <FaceRecognition />       */}
+    </div>
+  );
+}
+
+export default App;
