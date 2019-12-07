b5aaf97d8b559e19b02d7a8254679de5fb9d213d
thomasPendleton <57029458+thomasPendleton@users.noreply.github.com>
2019-12-06 23:35:43 -0800

    minor logo adjustment

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -9,7 +9,7 @@ const Logo = () => {
     return(
         <div className= 'ma4 mt0'>
             <Tilt className="Tilt br2 shadow-2" options={{ max : 50 }} style={{ height: 150, width: 150 }} >
-                <div className="Tilt-inner pa3"> <img style={{paddingTop: '5px'}} src={brain} alt="Logo"/>  </div>
+                <div className="Tilt-inner pa3"> <img style={{paddingTop: '10px'}} src={brain} alt="Logo"/>  </div>
             </Tilt>
         </div>
     )
